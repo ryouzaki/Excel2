e7062aac2eb0e053b8a0586f6b5913b179e76c0a
ryouzaki@mail.ru <45hgui032dkl>
2018-06-19 18:36:59 +0300

    Revert "pervy"
    This reverts commit a7d5446

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--operation', choices=['new', 'update'], required=True)
 parser.add_argument('-f', '--file', required=True)
 res = parser.parse_args()
-
-if (res.operation == 'new'):
+if res.operation == 'new':
     scripts.create_new_json(res.file)
-
+if res.operation == 'update':
+    scripts.update_json(res.file)
