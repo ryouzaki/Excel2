58791094ebcd2f9f7f01b4cec2b6789a60bf6b11
ryouzaki@mail.ru <45hgui032dkl>
2018-07-13 16:00:48 +0300

    Revert "pervy"
    This reverts commit a7d5446

--- plan_eater/scripts.py
+++ plan_eater/scripts.py
@@ -1,4 +1,5 @@
 from openpyxl import *
+from openpyxl.cell import Cell
 from openpyxl.formatting import Rule
 from openpyxl.formatting.rule import *
 from openpyxl.styles import Font, PatternFill, Border, Side, GradientFill
@@ -9,6 +10,8 @@ from openpyxl.worksheet.cell_range import CellRange
 import re
 import os
 import datetime
+import requests
+import feedparser
 from copy import copy
 
 
@@ -22,6 +25,7 @@ def create_new_json(plan):
             placement_dict.clear()
             weeknumber_set.clear()
             for column in range(2, mediaplan_sheet.max_column+1):
+
                 if mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet), column=column).value is not None:
                     if re.search('\d{1,2}', str(mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet), column=column).value)):
                         if mediaplan_sheet.cell(row=row, column=column).value == 1:
@@ -46,70 +50,48 @@ def create_new_json(plan):
 def update_json(plan):
     workbook = load_workbook(plan)
     mediaplan_sheet = workbook['MediaPlan']
-    weeknumber_set = set()
     for row in range(1, mediaplan_sheet.max_row):
         if re.search('\w\w_\w\w_\d\d\d', str(mediaplan_sheet.cell(row=row, column=1).value)):
-            with open(str(os.path.dirname(plan)) + '\\' + str(mediaplan_sheet.cell(row=row, column=1).value) + '.json',
-                      'r') as infile:
+            with open(str(os.path.dirname(plan)) + '\\' + str(mediaplan_sheet.cell(row=row, column=1).value) + '.json','r') as infile:
                 placement_dict = json.load(infile)
-            week_list = placement_dict['postclick']
-            for week in week_list:
-                if week['weeknumber'] > datetime.datetime.today().isocalendar()[1]:
-                    week_list.remove(week)
+            old_week_list = list(value for value in placement_dict['postclick'] if value['weeknumber'] < datetime.datetime.today().isocalendar()[1])
             for column in range(2, mediaplan_sheet.max_column + 1):
                 if mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet), column=column).value is not None:
-                    if re.search('\d{1,2}',
-                                 str(mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet), column=column).value)):
+                    if re.search('\d{1,2}',str(mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet), column=column).value)):
                         if (mediaplan_sheet.cell(row=row, column=column).value == 1) and (
-                                datetime.datetime.today().isocalendar()[1] <
-                                (mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet),
-                                                      column=column).value)):
-                            weeknumber_set.add(mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet),
-                                                                    column=column).value)
-                            print (mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet),
-                                                                    column=column).value)
+                        datetime.datetime.today().isocalendar()[1] <= (mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet),column=column).value)):
+                            if {'weeknumber': mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet),column=column).value, 'fact_budget': None, 'fact_impressions': None, 'fact_views': None,'fact_clicks': None, 'fact_reach': None} not in old_week_list:
+                                old_week_list.append({'weeknumber': mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet),column=column).value, 'fact_budget': None, 'fact_impressions': None, 'fact_views': None,'fact_clicks': None, 'fact_reach': None})
                         continue
-                    placement_dict.update({mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet),
-                                                                column=column).value: mediaplan_sheet.cell(row=row,
-                                                                                                           column=column).value})
-            for weeknumber in weeknumber_set:
-                week_list.append(
-                    {'weeknumber': weeknumber, 'fact_budget': None, 'fact_impressions': None, 'fact_views': None,
-                     'fact_clicks': None, 'fact_reach': None})
-            placement_dict.update({'postclick': week_list})
-            with open(str(os.path.dirname(plan)) + '\\' + str(mediaplan_sheet.cell(row=row, column=1).value) + '.json',
-                      'w') as outfile:
+                    placement_dict.update({mediaplan_sheet.cell(row=get_fields_row(mediaplan_sheet),column=column).value:
+                                               mediaplan_sheet.cell(row=row,column=column).value})
+            with open(str(os.path.dirname(plan)) + '\\' + str(mediaplan_sheet.cell(row=row, column=1).value) + '.json','w') as outfile:
                 json.dump(placement_dict, outfile)
 
 
 def parse_amnet():
-    report = load_workbook(os.getcwd()+'\Reports\\Amnet\\Amnet'+str(datetime.datetime.today().isocalendar()[1])+'.xlsx')
-    week_report_sheet = report[str(datetime.datetime.today().isocalendar()[1])]
+    week_number=datetime.datetime.today().isocalendar()[1]
+    report = load_workbook(os.getcwd()+'\Reports\\Amnet\\Amnet'+str(week_number)+'.xlsx')
+    week_report_sheet = report[str(week_number)]
     report_dict = dict()
     row = int()
     while row < week_report_sheet.max_row:
         row+=1
         if re.search('\w\w_\w\w_\d\d\d', str(week_report_sheet.cell(row=row, column=1).value)):
             placement_id = str(week_report_sheet.cell(row=row, column=1).value)
-            print (str(week_report_sheet.cell(row=row, column=1).value))
             while week_report_sheet.cell(row=row+1, column=1).value != None:
                 report_dict.update({str(week_report_sheet.cell(row=row+1, column=1).value): week_report_sheet.cell(row=row+1, column=2).value})
                 row+=1
-            print(report_dict)
             with open(str(os.getcwd()) + '\\MP\\' + placement_id + '.json', 'r+') as infile:
                 placement_dict = json.load(infile)
                 infile.seek(0)
                 infile.truncate()
                 week_list = placement_dict['postclick']
                 for week_info in week_list:
-                    if week_info['weeknumber'] == datetime.datetime.today().isocalendar()[1]:
-                        print (week_info['weeknumber'])
-                        print(report_dict)
+                    if week_info['weeknumber'] == week_number:
                         for stat in week_info.keys():
                             week_info.update({stat:report_dict.get(stat)})
-                            week_info.update({'weeknumber':datetime.datetime.today().isocalendar()[1]})
-                        print (week_info)
-                print (week_list)
+                            week_info.update({'weeknumber':week_number})
                 placement_dict.update({'postclick': week_list})
                 json.dump(placement_dict, infile)
                 report_dict.clear()
@@ -117,6 +99,7 @@ def parse_amnet():
 
 
 def create_report(week):
+    #список недель для формирования необходимых листов
     report = load_workbook(os.getcwd()+'\Reports\\'+ 'Template.xlsx')
     week_set = set()
     for jsonplan in os.listdir(os.getcwd() + '\\MP\\'):
@@ -124,22 +107,27 @@ def create_report(week):
             with open(str(os.getcwd()) + '\\MP\\' + jsonplan, 'r') as infile:
                 placement_dict = json.load(infile)
                 for value in placement_dict['postclick']:
-                    #if value['weeknumber'] <= datetime.datetime.today().isocalendar()[1]:
+                    if value['weeknumber'] <= datetime.datetime.today().isocalendar()[1]:
                         week_set.add(value['weeknumber'])
+
     for week in sorted(week_set):
-        fields_row=1
-        source = report.active
-        target = report.copy_worksheet(source)
-        first_stage_row= target.max_row+1
+        #начало переменных форматирования
         medium = Side(border_style="medium", color="000000")
         borders = Border (top=medium, left=medium, right=medium, bottom=medium)
-        while target.cell(row=fields_row,column=1).value != "fields":
-            fields_row+=1
+        yellow_fill = GradientFill(stop=("ffff99", "ffff99"))
+        black_fill = GradientFill(stop=("3b3b3b", "3b3b3b"))
+        text_rotation = Alignment(textRotation=90)
+        #конец переменных форматирования
+        source = report.active
+        target = report.copy_worksheet(source)
+        fields_row=get_fields_row(target)
+        first_stage_row= target.max_row+1
+
         for stage in ['Awareness','Consideration', 'Preference', 'Action', 'Loyalty']:
             if first_stage_row!=target.max_row+1:
-                target.cell(row=first_stage_row, column = 2).alignment = Alignment(textRotation=90)
+                target.cell(row=first_stage_row, column = 2).alignment = text_rotation
                 target.merge_cells(start_row=first_stage_row, end_row=target.max_row, start_column=2, end_column=2)
-                style_merged_cells(target,first_stage_row,target.max_row,2,2,border=borders, fill = GradientFill(stop=("ffff99", "ffff99")))
+                style_merged_cells(target,first_stage_row,target.max_row,2,2,border=borders, fill = yellow_fill)
                 first_stage_row=target.max_row+1
             for category in ['OLV', 'Programmatic', 'Social Media', 'SEA']:
                 category_flag = True
@@ -154,19 +142,12 @@ def create_report(week):
                                     target.cell(row=last_row,column=last_column+1, value = stage)
                                     target.merge_cells(start_row=last_row, start_column=last_column+2,end_row=last_row,end_column=target.max_column-1)
                                     target.cell(row=last_row,column=last_column+2, value = category)
-                                    style_merged_cells(target,last_row,last_row,last_column+2,target.max_column-1,border=borders,fill = GradientFill(stop=("ffff99", "ffff99")))
+                                    style_merged_cells(target,last_row,last_row,last_column+2,target.max_column-1,border=borders,fill = yellow_fill)
                                     category_flag = False
                                     last_row+=1
                                 while (target.cell(row=fields_row, column = last_column).value) != "end":
-                                    target.cell(row=last_row,column=last_column, value = placement_dict.get(str(target.cell(row=fields_row, column = last_column).value)))
-
-                                    if target.cell(row=fields_row, column = last_column).value == "period":
-                                        target.cell(row=last_row,column=last_column, value = str(len(placement_dict["postclick"])) + " weeks")
-
-                                    if re.search('\d{1,2}', str(target.cell(row=fields_row, column = last_column).value)):
-                                        for value in placement_dict['postclick']:
-                                            if value['weeknumber'] == target.cell(row=fields_row, column = last_column).value:
-                                                target.cell(row=last_row, column=last_column).fill = PatternFill(patternType='solid',fill_type='solid', fgColor = Color('3b3b3b'))
+                                    cell = get_value_from_placement_dict(target.cell(row=fields_row, column = last_column).value,placement_dict)
+                                    target.cell(row=last_row,column=last_column, value = cell.value)
 
                                     if target.cell(row=fields_row, column = last_column).value == "plan_impressions":
                                         target.cell(row=last_row, column=last_column, value = (placement_dict.get("plan_impressions")))
@@ -246,7 +227,6 @@ def create_report(week):
                                     if target.cell(row=fields_row, column = last_column).value == "fact_cpm":
                                         for value in placement_dict['postclick']:
                                             if value['weeknumber'] == week:
-                                                print (week)
                                                 target.cell(row=last_row, column=last_column, value = (value.get("fact_budget")*1000/value.get("fact_impressions")))
                                                 target.cell(row=last_row, column=last_column).number_format = '#,##0_-₽'
 
@@ -321,6 +301,30 @@ def get_fields_row(worksheet):
         fields_row+=1
     return fields_row
 
+def get_adriver_value():
+        url = 'https://api.adriver.ru/stat/profiles/2737205/total'
+        headers = {'content-type': 'application/atom+xml','X-Auth-UserID':'130529','X-Auth-Passwd':'%5C%A8%F2i%A7%8CD%98%12%F8I'}
+        r = requests.get(url = url, headers=headers)
+        feed = feedparser.parse(r.content)
+        print (feed.entries)
+        print (feed.entries[0]['adriver_exp'])
 
 
+def get_value_from_placement_dict(attrib, placement_dict):
+    wb = Workbook()
+    ws = wb.active
+    return_cell = Cell(ws)
+    return_cell.value == ''
+    if attrib == 'fields':
+        return_cell.value == ''
+        return return_cell
+    if attrib == 'platform_site' or attrib == 'description' or attrib == 'format':
+        return_cell.value = placement_dict.get(attrib)
+        return return_cell
+    return return_cell
+    # if attrib == 'period':
+    #     return str(len(placement_dict["postclick"])) + " weeks"
+    # if re.search('\d{1,2}', str(attrib)):
+    #     for x in (value for value in placement_dict['postclick'] if value['weeknumber'] == attrib):
+    #         return 1
 
